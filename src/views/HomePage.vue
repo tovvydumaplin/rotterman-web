3caf732153e733e12ff2860759d90f6b5c4f6ed9
Tovvy Dumaplin <tovvydumaplin@gmail.com>
2026-08-03 07:54:52 +0800

    Update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -6,7 +6,7 @@ import AboutSection from '../components/HomePage/AboutSection.vue'
 <template>
   <main>
     <!-- AAYUSIN PA LAYOUT DITO TANGA WAG KA MUNA SUMAWSAW PLS LANG BAKA MAPASOK KITA -->
-    <TheWelcome betlogByRicky="Betlog si Ricky Green" />
+    <TheWelcome betlogByRicky="Betlog si Dar Green" />
     <AboutSection />
   </main>
 </template>
